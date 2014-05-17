a9cc45aa5e236cd4d6afab3a3462a13ce46b17bf
Anton Rudeshko <anton@rudeshko.com>
2014-05-17 20:39:28 +0400

    Updated setup script

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-My dotfiles
-=======
+# My dotfiles
 
 Based on huge variety across GitHub. Kudos to @veged, @mishanga, @paulirish, @addyosmani.
+
+## Install
+
+```bash
+cd && git clone https://github.com/anton-rudeshko/configs.git && ~/configs/setup.bash
+```
--- setup.bash
+++ setup.bash
@@ -1,4 +1,45 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ $OSTYPE =~ darwin ]]; then
+    echo "OS X detected"
+    if [ -n `which brew` ]; then
+        echo "Installing Homebrew..."
+        ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+
+        # brew bundle ~/configs/Brewfile
+        # brew bundle ~/configs/Caskfile
+    fi
+fi
+
+echo "Symlinking configs..."
 for file in .{profile,gitconfig,vimrc,inputrc}; do
     ln -fs ~/configs/$file ~/$file
 done
 unset file
+
+CURRENT_GIT_USER=`git config --global --get user.name`
+CURRENT_GIT_EMAIL=`git config --global --get user.email`
+
+echo "Configuring git..."
+read -p "Enter your full name ($CURRENT_GIT_USER): " GIT_USER
+read -p "Enter your e-mail ($CURRENT_GIT_EMAIL): " GIT_EMAIL
+
+GIT_USER=${GIT_USER:-$CURRENT_GIT_USER}
+GIT_EMAIL=${GIT_EMAIL:-$CURRENT_GIT_EMAIL}
+
+echo "Sourcing new profile..."
+source ~/.profile
+
+cat >~/.extra <<EOL
+GIT_AUTHOR_NAME="$GIT_USER"
+GIT_COMMITTER_NAME="$GIT_USER"
+git config --global user.name "$GIT_USER"
+
+GIT_AUTHOR_EMAIL="$GIT_EMAIL"
+GIT_COMMITTER_EMAIL="$GIT_EMAIL"
+git config --global user.email "$GIT_EMAIL"
+EOL
+
+echo "Done."
