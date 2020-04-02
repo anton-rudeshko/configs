e93b341f66ae20722f1c98c191050bf1492ee090
Anton Rudeshko <anton@rudeshko.com>
2020-04-02 19:16:49 +0300

    setup: move vim config to separate dir

---
+++ vim/vim.sh
@@ -0,0 +1,19 @@
+vundle_dir="${HOME}/.vim/bundle/Vundle.vim"
+
+if [ ! -d "${vundle_dir}" ]; then
+    read -p "Install vundle? (y/n) " -n 1 -r
+    echo
+    if [[ ${REPLY} =~ ^[Yy]$ ]]; then
+        echo "vim: installing Vundle..."
+        mkdir -p "${vundle_dir}"
+        git clone "https://github.com/VundleVim/Vundle.vim.git" "${vundle_dir}"
+    fi
+fi
+
+if [ -d "${vundle_dir}" ]; then
+    echo "vim: running Vundle:PluginInstall..."
+    vim -c 'PluginInstall' -c 'qa!'
+fi
+
+echo "vim: symlinking vimrc..."
+ln -sf "${dotfiles_dir}/vimrc" "${HOME}/.vimrc"
