1521c8a1bd000c9b63cd70baf54f5aead8d11f69
Anton Rudeshko <rudeshko@yandex-team.ru>
2020-04-07 14:06:05 +0300

    choosy: add behaviour rules

--- macos/choosy/setup.sh
+++ macos/choosy/setup.sh
@@ -1,3 +1,6 @@
 current_dir=$(cd "$(dirname "$0")" && pwd)
 
 defaults import com.choosyosx.Choosy "${current_dir}/com.choosyosx.Choosy.plist"
+
+mkdir -p "${HOME}/Library/Application Support/Choosy/"
+cp -f "${current_dir}/behaviours.plist" "${HOME}/Library/Application Support/Choosy/behaviours.plist"
