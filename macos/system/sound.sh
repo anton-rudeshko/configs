5a75121d87486097fb19069e4823570d805de204
Anton Rudeshko <rudeshko@yandex-team.ru>
2020-04-07 13:26:32 +0300

    macos: disable sound on charge

--- macos/system/sound.sh
+++ macos/system/sound.sh
@@ -6,3 +6,7 @@ defaults write NSGlobalDomain com.apple.sound.beep.feedback -int 0
 defaults write NSGlobalDomain com.apple.sound.beep.flash -int 0
 defaults write NSGlobalDomain com.apple.sound.beep.sound -string "/System/Library/Sounds/Tink.aiff"
 defaults write NSGlobalDomain com.apple.sound.uiaudio.enabled -int 0
+
+# Disable power chime
+defaults write com.apple.PowerChime ChimeOnAllHardware -bool false
+defaults write com.apple.PowerChime ChimeOnNoHardware -bool true
