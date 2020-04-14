ae1e4c2b2601c14b145824a75da9410040dce919
Anton Rudeshko <anton@rudeshko.com>
2020-04-14 22:22:51 +0300

    ui: remove annoying notification about iCloud

--- macos/system/ui.sh
+++ macos/system/ui.sh
@@ -65,3 +65,6 @@ defaults write com.apple.helpviewer DevMode -bool true
 # Reveal IP address, hostname, OS version, etc. when clicking the clock
 # in the login window
 sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
+
+# Remove annoying notification about 'Start using iCloud'
+defaults delete com.apple.systempreferences AttentionPrefBundleIDs
