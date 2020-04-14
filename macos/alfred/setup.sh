1c7bd4b81495d89ecd7e57998d9ab2401545e8ad
Anton Rudeshko <anton@rudeshko.com>
2020-04-14 22:21:32 +0300

    alfred: add setup script

---
+++ macos/alfred/setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+defaults write com.runningwithcrayons.Alfred-Preferences-3 hidev4messagebanner -bool true
+defaults write com.runningwithcrayons.Alfred-Preferences-3 syncfolder -string "~/Yandex.Disk.localized/settings/alfred"
+defaults write com.runningwithcrayons.Alfred-Preferences-3 themeEditorBackground -string "Yosemite.jpg"
+defaults write com.runningwithcrayons.Alfred-Preferences-3 workflows.showCategories -bool false
+defaults write com.runningwithcrayons.Alfred-Preferences-3 workflows.showHotkeys -bool false
